d2f20d447e01ff28663e1643e09a701102e5f456
Steven Choy <stevenchoy0915@gmail.com>
2024-08-12 16:25:14 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import data from "./assets/data.json";
 
 function App() {
+  const [users, setUsers] = useState(data);
+  const [show, setShow] = useState(false);
+  const [sender, setSender] = useState("");
+  const [receiver, setReceiver] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const userExist = (name) => {};
+
+  const findUser = (name) => {};
+
+  const transferMoney = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        <div>
+          {users.map((user) => {
+            return (
+              <div key={user.id}>
+                {" "}
+                {user.name} - {user.balance}
+              </div>
+            );
+          })}
+        </div>
+      }
+      {
+        <form onSubmit={transferMoney}>
+          <label>Sender:</label>
+          <input
+            type="text"
+            value={sender}
+            onChange={(event) => setSender(event.target.value)}
+            required
+          ></input>
+          <br />
+          <input
+            type="text"
+            value={receiver}
+            onChange={(event) => setreceiver(event.target.value)}
+            required
+          ></input>
+          <br />
+          <input
+            type="nummber"
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+            required
+          ></input>
+          <br />
+          <button>Confirm Transfer</button>
+        </form>
+      }
     </div>
   );
 }
